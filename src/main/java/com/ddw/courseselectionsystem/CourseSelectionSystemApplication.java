b474e598763cdcd9e5bd6c9aeaec456e9f2d35f7
dengdingwen <dengdingwen001@ke.com>
2018-12-04 15:23:17 +0800

    first commit

---
+++ src/main/java/com/ddw/courseselectionsystem/CourseSelectionSystemApplication.java
@@ -0,0 +1,12 @@
+package com.ddw.courseselectionsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSelectionSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseSelectionSystemApplication.class, args);
+    }
+}
